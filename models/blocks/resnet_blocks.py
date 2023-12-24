9a56e1b244caef0ab3ac891fc36f82ec848fe4ad
Azatgal <azat_gal@icloud.com>
2023-12-24 14:55:37 +0300

    test

--- models/blocks/resnet_blocks.py
+++ models/blocks/resnet_blocks.py
@@ -56,6 +56,7 @@ class Bottleneck(nn.Module):
         self.expansion = expansion
         self.down_sampling = down_sampling
 
+        """
         self.path_A = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=(1, 1), stride=(stride, stride), padding=1),
             nn.BatchNorm2d(out_channels),
@@ -66,7 +67,18 @@ class Bottleneck(nn.Module):
             nn.Conv2d(out_channels, out_channels * expansion, kernel_size=(1, 1)),
             nn.BatchNorm2d(out_channels * expansion)
         )
+        """
+        self.path_A = []
+        self.path_A.append(nn.Conv2d(in_channels, out_channels, kernel_size=(1, 1), stride=(stride, stride), padding=1))
+        self.path_A.append(nn.BatchNorm2d(out_channels))
+        self.path_A.append(nn.ReLU(inplace=True))
+        self.path_A.append(nn.Conv2d(out_channels, out_channels, kernel_size=(3, 3), padding=1))  # , stride=stride, padding=1),
+        self.path_A.append(nn.BatchNorm2d(out_channels))
+        self.path_A.append(nn.ReLU(inplace=True))
+        self.path_A.append(nn.Conv2d(out_channels, out_channels * expansion, kernel_size=(1, 1)))
+        self.path_A.append(nn.BatchNorm2d(out_channels * expansion))
 
+        """
         if self.down_sampling:
             self.path_B = nn.Sequential(
                 nn.Conv2d(in_channels, out_channels * expansion, kernel_size=(1, 1), stride=(stride, stride), padding=1),
@@ -74,14 +86,31 @@ class Bottleneck(nn.Module):
             )
         else:
             self.path_B = nn.Identity()
+        """
+        self.path_B = []
+        if self.down_sampling:
+            self.path_B.append(nn.Conv2d(in_channels, out_channels * expansion, kernel_size=(1, 1),
+                                         stride=(stride, stride), padding=1))
+            self.path_B.append(nn.BatchNorm2d(out_channels * expansion))
+        else:
+            self.path_B.append(nn.Identity())
 
     def forward(self, inputs):
         # TODO: реализуйте forward pass
         # raise NotImplementedError
+        """
         x = self.path_A(inputs)
         x += self.path_B(inputs)
         x = nn.ReLU(inplace=True)(x)
-        return x
+        """
+        x = inputs
+        y = inputs
+        for i in self.path_A:
+            x = i(x)
+        for i in self.path_B:
+            y = i(y)
+        outputs = nn.ReLU(inplace=True)(x + y)
+        return outputs
 
 
 class Stage(nn.Module):
