82cbbecaa002285dabbc51769e23eedf93f3de20
Azatgal <azat_gal@icloud.com>
2023-12-24 16:21:21 +0300

    test

--- models/blocks/resnet_blocks.py
+++ models/blocks/resnet_blocks.py
@@ -81,7 +81,7 @@ class Bottleneck(nn.Module):
             nn.Conv2d(out_channels, out_channels, kernel_size=(3, 3), padding=1),  # , stride=stride, padding=1),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels * expansion, kernel_size=(1, 1)),
+            nn.Conv2d(out_channels, out_channels * expansion, kernel_size=(1, 1), padding=1),
             nn.BatchNorm2d(out_channels * expansion)
         )
         """
