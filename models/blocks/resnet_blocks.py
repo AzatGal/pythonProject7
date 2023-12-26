dd742636a04c9a74f1c5ec732cbf6c1d2532884d
Azatgal <azat_gal@icloud.com>
2023-12-26 10:26:25 +0300

    test

--- models/blocks/resnet_blocks.py
+++ models/blocks/resnet_blocks.py
@@ -34,7 +34,7 @@ class InputStemC(nn.Module):
         """
         super().__init__()
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(3, 3), stride=(2, 2), padding=2)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3))  # , padding=1)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=1)
         self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=2)
         self.max_pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=2)
         # raise NotImplementedError
