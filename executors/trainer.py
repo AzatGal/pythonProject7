f28500472d3e5ab1a68ed64e2f336b66546c03bb
Azatgal <azat_gal@icloud.com>
2023-12-27 21:56:44 +0300

    test

--- executors/trainer.py
+++ executors/trainer.py
@@ -121,7 +121,7 @@ class Trainer:
         if self.cfg.model_name in ("ResNetB", "ResNetC", "ResNetD"):
             self.scheduler_wu = LambdaLR(
                 self.optimizer,
-                lr_lambda=lambda epoch: (epoch + 1) * self.cfg.lr / 10 if epoch < 5 else self.cfg.lr
+                lr_lambda=lambda epoch: (epoch + 1) * self.cfg.lr / 5 if epoch < 2 else self.cfg.lr
             )
             self.scheduler_cd = CosineAnnealingLR(self.optimizer, T_max=self.cfg.num_epochs)
 
